f80f2695f0a7a976340e66e9f54c142c12bab382
Rob Haswell <me@robhaswell.co.uk>
2020-01-03 09:33:59 +0000

    Enable Link Time Optimization.

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -120,6 +120,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define NO_ACTION_MACRO
 // #define NO_ACTION_FUNCTION
 
+#define LINK_TIME_OPTIMIZATION_ENABLE
+
 #define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_LED_PROCESS_LIMIT 15
 #define RGB_MATRIX_LED_FLUSH_LIMIT 10
