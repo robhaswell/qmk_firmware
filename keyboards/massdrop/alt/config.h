32840ff846f228bf6b48a05fd6ff50f12732c912
Rob Haswell <me@robhaswell.co.uk>
2020-01-17 16:45:03 +0000

    Remove example custom effect.

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -125,6 +125,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_LED_FLUSH_LIMIT 10
 
 // Load our custom RGB by default
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CUSTOM_my_cool_effect
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CUSTOM_CUSTOM_EFFECT
 
 #include "config_led.h"
