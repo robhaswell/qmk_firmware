29ca79ec4c6d475801d1fe1110ee34cf56353c18
Rob Haswell <me@robhaswel.co.uk>
2020-08-03 09:30:43 +0100

    OSX keymap.

--- keyboards/lily58/keymaps/custom/keymap.c
+++ keyboards/lily58/keymaps/custom/keymap.c
@@ -16,8 +16,9 @@ extern rgblight_config_t rgblight_config;
 extern uint8_t is_master;
 
 enum custom_layers {
-    _QWERTY = 0,
-    _SHIFT,
+    _MAC = 0,
+    _WIN,
+    _WINSHIFT,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -32,14 +33,21 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_QWERTY] = LAYOUT( \
-  KC_ESC,                KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,                KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NUBS, \
-  KC_LCTRL,              KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  LM(_SHIFT, MOD_LSFT),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  LM(_SHIFT, MOD_RSFT), \
-                                          KC_MINS, KC_LALT, KC_LGUI, KC_SPC,   KC_ENT,   LOWER,   RAISE,   KC_EQL \
+[_MAC] = LAYOUT( \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                             KC_MINS, KC_LALT, KC_LGUI, KC_SPC,   KC_ENT,   LOWER,   RAISE,   KC_EQL \
 ),
-[_SHIFT] = LAYOUT( \
+[_WIN] = LAYOUT( \
+  KC_ESC,                   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,                   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NUBS, \
+  KC_LCTRL,                 KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  LM(_WINSHIFT, MOD_LSFT),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  LM(_WINSHIFT, MOD_RSFT), \
+                                             KC_MINS, KC_LALT, KC_LGUI, KC_SPC,   KC_ENT,   LOWER,   RAISE,   KC_EQL \
+),
+[_WINSHIFT] = LAYOUT( \
   _______, _______, KC_QUOT, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_2, \
@@ -133,7 +141,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_MAC);
       }
       return false;
       break;
