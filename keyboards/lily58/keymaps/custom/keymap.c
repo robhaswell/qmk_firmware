1203505f026111fac81b6818e4bfa83a6223b7b2
Rob Haswell <me@robhaswel.co.uk>
2020-08-04 10:35:44 +0100

    Move volume keys so that they do not interfere with right shift.

--- keyboards/lily58/keymaps/custom/keymap.c
+++ keyboards/lily58/keymaps/custom/keymap.c
@@ -65,20 +65,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_PGUP, \
   _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MPLY, KC_PGDN, \
-  _______, _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, KC_P1,   KC_P2,   KC_P3,   _______, KC_VOLU, \
-  _______, _______, _______, _______, _______, KC_LCBR, _______, _______, KC_RCBR, KC_P0,   _______, _______, _______, KC_VOLD, \
+  _______, _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, KC_P1,   KC_P2,   KC_P3,   _______, XXXXXXX, \
+  _______, _______, _______, _______, _______, KC_LCBR, _______, _______, KC_RCBR, KC_P0,   _______, _______, _______, _______, \
                              KC_HOME, _______, _______, _______, _______, _______, _______, KC_END \
 ),
 [_RAISE] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  LCTL(KC_PGUP), \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, LCTL(KC_PGDN), \
-  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,                   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,                   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, \
                              KC_F11,  _______, _______, _______, _______, _______, _______, KC_F12 \
 ),
 [_ADJUST] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRMU, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRMD, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
