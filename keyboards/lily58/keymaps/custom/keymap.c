6be45e5ef0a82c11dc88ed05ac19773e827d21a8
Rob Haswell <me@robhaswel.co.uk>
2020-08-20 15:01:54 +0100

    Add more conditions where the encoder state is not reset.

--- keyboards/lily58/keymaps/custom/keymap.c
+++ keyboards/lily58/keymaps/custom/keymap.c
@@ -44,16 +44,17 @@ uint8_t encoder_mode      = 0;
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL   (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
 #define MODS_ALT    (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+#define MODS_GUI    (get_mods() & MOD_BIT(KC_LGUI) || get_mods() & MOD_BIT(KC_RGUI))
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_MAC] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                  ENCODER,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_GRV,  LM(_MACGUI, MOD_LGUI), LOWER,   KC_SPC,   KC_ENT,   RAISE,   KC_LALT, KC_BSLS \
+                             KC_GRV,  KC_LGUI, LOWER,   KC_SPC,   KC_ENT,   RAISE,   KC_LALT, KC_BSLS \
 ),
 [_MACGUI] = LAYOUT( \
   KC_GRV,  _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
@@ -199,18 +200,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 
     // Reset the encoder mode if any alpha is pressed
-    switch (keycode) {
-        case KC_LSFT:
-        case KC_RSFT:
-        case KC_LCTRL:
-        case KC_RCTRL:
-        case KC_LALT:
-        case KC_RALT:
-        case LOWER:
-        case RAISE:
-            break;
-        default:
-            encoder_mode = 0;
+    if (record->event.pressed) {
+        switch (keycode) {
+            case KC_LSFT:
+            case KC_RSFT:
+            case KC_LCTRL:
+            case KC_LALT:
+            case KC_LGUI:
+            case LOWER:
+            case RAISE:
+                break;
+            default:
+                if (MODS_SHIFT || MODS_ALT || MODS_CTRL || MODS_GUI)
+                    // Do not reset if any modifiers are held
+                    break;
+                dprint("resetting encoder\n");
+                encoder_mode = 0;
+        }
     }
 
     switch (keycode) {
