83702e11320a64219e1f6a5a21f7ac2238d89ac5
Rob Haswell <me@robhaswel.co.uk>
2020-09-03 18:05:16 +0100

    Play on the raise layer for one-handed operation.

--- keyboards/lily58/keymaps/custom/keymap.c
+++ keyboards/lily58/keymaps/custom/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  LGUI(LSFT(KC_LBRC)), \
-  _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, _______, KC_VOLD, KC_VOLU, _______, LGUI(LSFT(KC_RBRC)), \
+  _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, _______, KC_VOLD, KC_VOLU, KC_MPLY, LGUI(LSFT(KC_RBRC)), \
   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC,                   KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
   _______, _______, _______, _______, _______, KC_LCBR, _______, KC_DEL,  KC_RCBR, _______, _______, _______, _______, _______, \
                              KC_F11,  _______, _______, _______, _______, _______, _______, KC_F12 \
