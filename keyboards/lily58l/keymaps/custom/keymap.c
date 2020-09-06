ef55bcb05184c3f152b7887557808a0cf0fc9f7f
Rob Haswell <me@robhaswel.co.uk>
2020-09-06 21:25:27 +0100

    Latest version of the keymap which was not copied from my laptop.

--- keyboards/lily58l/keymaps/custom/keymap.c
+++ keyboards/lily58l/keymaps/custom/keymap.c
@@ -17,7 +17,6 @@ extern uint8_t is_master;
 
 enum custom_layers {
     _MAC = 0,
-    _MACGUI,
     _WIN,
     _WINSHIFT,
     _LOWER,
@@ -50,18 +49,11 @@ uint8_t encoder_mode      = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_MAC] = LAYOUT( \
-  KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                  ENCODER,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_GRV,  KC_LGUI, LOWER,   KC_SPC,   KC_ENT,   RAISE,   KC_LALT, KC_BSLS \
-),
-[_MACGUI] = LAYOUT( \
-  KC_GRV,  _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______,\
-                             _______, _______, _______, _______, _______, _______, _______, _______ \
+  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  ENCODER, KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                             KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_ENT,   RAISE,   KC_RALT, KC_BSLS \
 ),
 [_WIN] = LAYOUT( \
   KC_ESC,                   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
@@ -78,15 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 [_LOWER] = LAYOUT( \
-  KC_LOCK, _______, _______, LALT(KC_3),_______,_______,                  _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_PGUP, \
+  KC_GRV,  _______, _______, LALT(KC_3),_______,_______,                  _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_PGUP, \
   _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MPLY, KC_PGDN, \
   _______, _______, _______, _______, _______, KC_LBRC,                   KC_RBRC, KC_P1,   KC_P2,   KC_P3,   _______, XXXXXXX, \
   _______, _______, _______, _______, _______, KC_LCBR, _______, KC_DEL,  KC_RCBR, KC_P0,   _______, _______, _______, _______, \
                              KC_HOME, _______, _______, KC_LALT, _______, _______, _______, KC_END \
 ),
 [_RAISE] = LAYOUT( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  LGUI(LSFT(KC_LBRC)), \
-  _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, _______, KC_VOLD, KC_VOLU, _______, LGUI(LSFT(KC_RBRC)), \
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  LGUI(LSFT(KC_LBRC)), \
+  _______, _______, _______, _______, _______, KC_LPRN,                   KC_RPRN, _______, KC_VOLD, KC_VOLU, KC_MPLY, LGUI(LSFT(KC_RBRC)), \
   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC,                   KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
   _______, _______, _______, _______, _______, KC_LCBR, _______, KC_DEL,  KC_RCBR, _______, _______, _______, _______, _______, \
                              KC_F11,  _______, _______, _______, _______, _______, _______, KC_F12 \
