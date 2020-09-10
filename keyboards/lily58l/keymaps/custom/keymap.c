2dfda7d7126ad5652f441d4c04bcd2b95673c753
Rob Haswell <me@robhaswel.co.uk>
2020-09-10 10:49:58 +0100

    Bring Windows mode back into line with Mac mode

--- keyboards/lily58l/keymaps/custom/keymap.c
+++ keyboards/lily58l/keymaps/custom/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,                   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
   KC_LCTRL,                 KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   LM(_WINSHIFT, MOD_LSFT),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    ENCODER,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  LM(_WINSHIFT, MOD_RSFT), \
-                                             KC_GRV,  KC_LGUI, KC_LALT, KC_SPC,   KC_ENT,   LOWER,   RAISE,   KC_NUBS \
+                                             KC_LGUI, KC_LALT, LOWER,   KC_SPC,   KC_ENT,   RAISE,   KC_RGUI, KC_NUBS \
 ),
 [_WINSHIFT] = LAYOUT( \
   _______, _______, KC_QUOT, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
